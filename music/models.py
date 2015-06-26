7ff6f82fa55c6493c79a579ea327ad227cd63a25
Steve Johnson <sjohnson0684@gmail.com>
2015-06-26 13:53:19 -0400

    Initial music model work

--- music/models.py
+++ music/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+from common.models import AbstractBaseModel
+
+
+class InstrumentGroup(AbstractBaseModel):
+    pass
+
+
+class Instrument(AbstractBaseModel):
+    instrument_group = models.ForeignKey(InstrumentGroup)
