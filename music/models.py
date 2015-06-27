6f9ddc768612bf7bdf82cda24be2ef5526ba462e
Steve Johnson <sjohnson0684@gmail.com>
2015-06-27 16:50:04 -0400

    Music model work with tests

--- music/models.py
+++ music/models.py
@@ -1,11 +1,24 @@
 from django.db import models
 
-from common.models import AbstractBaseModel
+from common.models import AbstractBaseModel, AbstractMPTTBaseModel
 
 
-class InstrumentGroup(AbstractBaseModel):
-    pass
+class InstrumentGroup(AbstractMPTTBaseModel):
+    name = models.CharField(max_length=100, unique=True)
+
+    @property
+    def all_instruments(self):
+        return self._get_all_instruments()
+
+    def _get_all_instruments(self):
+        """
+        Retrieves all active instrument records that are a part of this group and its descendant groups.
+        """
+        descendants = self.get_descendants(include_self=True)
+        descendant_pks = [descendant.pk for descendant in descendants if descendant.is_active]
+        return Instrument.objects.filter(instrument_group__in=descendant_pks).exclude(is_active=False)
 
 
 class Instrument(AbstractBaseModel):
+    name = models.CharField(max_length=100, unique=True)
     instrument_group = models.ForeignKey(InstrumentGroup, related_name='instruments')
--- music/tests.py
+++ music/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import InstrumentGroup, Instrument
+
+
+class InstrumentGroupTests(TestCase):
+
+    def test_get_all_instruments(self):
+        instrument_group_1 = InstrumentGroup.objects.create(name='Brass')
+        instrument_group_2 = InstrumentGroup.objects.create(name='Low Brass', parent=instrument_group_1)
+        Instrument.objects.create(name='Trumpet', instrument_group=instrument_group_1)
+        Instrument.objects.create(name='Tuba', instrument_group=instrument_group_2)
+        self.assertEqual(instrument_group_1.all_instruments.count(), 2)
