5f1d33ff5bbf5b9be4c5adbf07ab822a81343684
Steve Johnson <sjohnson0684@gmail.com>
2015-06-27 15:40:24 -0400

    Music app added to reset scripts

--- reset_database.bat
+++ reset_database.bat
@@ -3,6 +3,7 @@
 del "db.sqlite3"
 del "content\migrations\0001_initial.py"
 del "members\migrations\0001_initial.py"
+del "music\migrations\0001_initial.py"
 python manage.py makemigrations
 python manage.py migrate
 python manage.py createsuperuser
--- reset_database.sh
+++ reset_database.sh
@@ -2,6 +2,7 @@
 rm db.sqlite3
 rm content/migrations/0001_initial.py
 rm members/migrations/0001_initial.py
+rm music/migrations/0001_initial.py
 python manage.py makemigrations
 python manage.py migrate
 python manage.py createsuperuser
