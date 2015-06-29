a6bffef0f285f493437d045cd1d61cd4f0a95b18
Steve Johnson <sjohnson0684@gmail.com>
2015-06-29 13:28:31 -0400

    Admin registration of music models

--- music/admin.py
+++ music/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Instrument, InstrumentGroup, Composer, MusicalWork, MusicalWorkCategory, Rehearsal, Concert,\
+    ConcertType
+
+admin.site.register(Instrument)
+admin.site.register(InstrumentGroup)
+admin.site.register(Composer)
+admin.site.register(MusicalWork)
+admin.site.register(MusicalWorkCategory)
+admin.site.register(Rehearsal)
+admin.site.register(Concert)
+admin.site.register(ConcertType)
