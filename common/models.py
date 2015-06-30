3f6aa402e729d1fbf352b082b2a6b7c7ebcfd25c
Steve Johnson <sjohnson0684@gmail.com>
2015-06-30 16:56:53 -0400

    Discovered the cached_property function
    Django has this already implemented under django.utils.functional as
    cached_property

--- common/models.py
+++ common/models.py
@@ -10,13 +10,6 @@ class AbstractBaseModel(models.Model):
     created_at = models.DateTimeField('creation time', auto_now_add=True)
     updated_at = models.DateTimeField('update time', auto_now=True)
 
-    def __init__(self, *args, **kwargs):
-        """
-        Constructor override adds property cache to model instance.
-        """
-        super(AbstractBaseModel, self).__init__(*args, **kwargs)
-        self._property_cache = dict()
-
     def pre_save(self, is_new):
         """
         Code that is executed before the save method.
@@ -37,13 +30,11 @@ class AbstractBaseModel(models.Model):
 
     def save(self, *args, **kwargs):
         """
-        Save override resets property cache on model instance.
-        Also handles pre/post save events
+        Save override handles pre/post save events.
         """
         is_new = False if self.pk else True
         self.pre_save(is_new)
         super(AbstractBaseModel, self).save(*args, **kwargs)
-        self._property_cache = dict()
         self.post_save(is_new)
 
     def delete(self, *args, **kwargs):
@@ -52,16 +43,6 @@ class AbstractBaseModel(models.Model):
         """
         self.pre_delete()
         super(AbstractBaseModel, self).delete(*args, **kwargs)
-
-    def _get_property(self, name, setter):
-        """
-        Retrieves cached property.
-        """
-        try:
-            return self._property_cache[name]
-        except KeyError:
-            self._property_cache[name] = setter()
-            return self._property_cache[name]
 
     class Meta:
         abstract = True
