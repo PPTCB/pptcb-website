81e4f9d2f6d53cb1ffae3dd1bf7f4d07a4f5474d
Steve Johnson <sjohnson0684@gmail.com>
2015-07-01 16:23:39 -0400

    Repalced createsuperuser with database seed command

--- reset_database.bat
+++ reset_database.bat
@@ -6,4 +6,4 @@ del "members\migrations\0001_initial.py"
 del "music\migrations\0001_initial.py"
 python manage.py makemigrations
 python manage.py migrate
-python manage.py createsuperuser
+python manage.py dbseed --test
--- reset_database.sh
+++ reset_database.sh
@@ -5,4 +5,4 @@ rm members/migrations/0001_initial.py
 rm music/migrations/0001_initial.py
 python manage.py makemigrations
 python manage.py migrate
-python manage.py createsuperuser
+python manage.py dbseed --test
