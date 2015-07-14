4fa16b2c54946ebdec9b319c3f0a1d7196515160
Steve Johnson <sjohnson0684@gmail.com>
2015-07-14 16:04:40 -0400

    Music library table

--- music/views.py
+++ music/views.py
@@ -1,13 +1,22 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from .models import MusicalWork
+
 
 def concerts(request):
     return HttpResponse('concerts')
 
 
 def library(request):
-    return render(request, 'members/music/library.html')
+    context = dict()
+
+    # Get active musical works, ordered by their library IDs
+    context['musical_works'] = MusicalWork.objects.select_related('category')\
+        .prefetch_related('composers', 'arrangers').filter(is_active=True).order_by('library_id')
+
+    # Render template
+    return render(request, 'members/music/library.html', context)
 
 
 def instruments(request):
