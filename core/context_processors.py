e9cc1d65bb670d3b21095b16dd00dd7638bb55e1
Steve Johnson <sjohnson0684@gmail.com>
2015-07-14 15:09:59 -0400

    Full URL name context processor added

--- core/context_processors.py
+++ core/context_processors.py
@@ -1,5 +1,11 @@
 from django.conf import settings
 
 
+def full_url_name(request):
+    name_list = request.resolver_match.namespaces
+    name_list.append(request.resolver_match.url_name)
+    return {'full_url_name': ':'.join(name_list)}
+
+
 def installed_apps(request):
-    return {'installed_apps' : settings.INSTALLED_APPS}
+    return {'installed_apps': settings.INSTALLED_APPS}
--- primetimeband/settings.py
+++ primetimeband/settings.py
@@ -68,6 +68,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'core.context_processors.full_url_name',
                 'core.context_processors.installed_apps',
             ],
         },
