06d45b309832d76a78d0ed8adfad979fb303c15e
Steve Johnson <sjohnson0684@gmail.com>
2015-07-14 13:55:10 -0400

    URL name fix for concerts

--- music/urls.py
+++ music/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import url
 from .import views
 
 urlpatterns = [
-    url(r'^concerts$', views.concerts, name='concerts$'),
+    url(r'^concerts$', views.concerts, name='concerts'),
     url(r'^library$', views.library, name='library'),
     url(r'^instruments$', views.instruments, name='instruments'),
 ]
