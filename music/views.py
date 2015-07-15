7c1be8f8a47b3c5ce424a6a60285aed0fbee1647
Steve Johnson <sjohnson0684@gmail.com>
2015-07-15 11:54:09 -0400

    Add musical work form

--- music/views.py
+++ music/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
-from .models import MusicalWork
+from .models import MusicalWork, MusicalWorkCategory
 
 
 def concerts(request):
@@ -13,7 +13,21 @@ def library(request):
 
     # Get active musical works, ordered by their library IDs
     context['musical_works'] = MusicalWork.objects.select_related('category')\
-        .prefetch_related('composers', 'arrangers').filter(is_active=True).order_by('library_id')
+        .prefetch_related('composers', 'arrangers').order_by('library_id')
+
+    # Get next library ID
+    if context['musical_works'].count() > 0:
+        context['musical_works_next_library_id'] = context['musical_works'].reverse()[0].library_id + 1
+    else:
+        context['musical_works_next_library_id'] = 1
+
+    # Get grade level choices
+    context['musical_work_grade_choices'] = [{'display': choice[1], 'value': choice[0]}
+                                             for choice in MusicalWork.GRADE_CHOICES]
+
+    # Get pre-existing categories
+    context['musical_work_categories'] = [{'display': category.name, 'value': category.pk} for category in
+                                          MusicalWorkCategory.objects.filter(is_active=True).order_by('name')]
 
     # Render template
     return render(request, 'members/music/library.html', context)
